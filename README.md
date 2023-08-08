4b7da0fee0e6784b7eba6db6e59e04182d10e14f
digitalready-peter <peter@digitalready.org>
2023-08-08 14:26:51 -0400

    Co-authored-by: digitalready-Dariel <digitalready-Dariel@users.noreply.github.com> Co-authored-by: digitalready-deven <digitalready-deven@users.noreply.github.com> Co-authored-by: digitalready-ruthson <digitalready-ruthson@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # simple old-maid card game
+# Create deck total of  51 cards and split for 4 players
+
+    #Shuffle/ randomize deck
+
+    #check if there are pairs in hands dump cards ( face down at your side )
+
+    # check if pairs in hands 
+
+    # dump pairs 
+
+# dont disclose card information
+
+
+
+
+
+# go in clockwise circle, player going chooses from player before them (take user input).
+
+    # player chooses from prevoius player before them/ clockwise rotation
+
+    # whenver player has pairs dump, continue untill player has odd amount of card = player loses
+
+    # pairs detected == dump 
+
+    # continue game until one player has old_maid_card 
---
+++ player.py
@@ -0,0 +1,120 @@
+#ruthson
+import random
+
+
+class  Card():
+       def new_deck():
+        deck = []
+        for suit in Card.SUITS:
+            for value in Card.VALUES:
+                deck.append(Card(value, suit))
+        random.shuffle(deck)
+        return deck
+    
+    VALUES = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    SUITS = ["Clubs", "Spades", "Diamonds", "Hearts"]
+
+def __init__(self, value, suit):
+    self.value = value
+    self.suit = suit 
+
+ def __repr__(self):
+        # ♣ ♦ ♥ ♠
+        if self.suit == "Clubs":
+            symbol = "♣"
+        elif self.suit == "Spades":
+            symbol = "♠"
+        elif self.suit == "Diamonds":
+            symbol = "♦"
+        elif self.suit == "Hearts":
+            symbol = "♥"
+        else:
+            raise Exception("Invalid Suit")
+
+        if self.value == 11:
+            val = "J"
+        elif self.value == 12:
+            val = "Q"
+        elif self.value == 13:
+            val = "K"
+        elif self.value == 1:
+            val = "A"
+        else: val = self.value
+
+        return f"{symbol}{val}"
+
+
+
+#whenver it is "users" current turn shuffle that users deck proceed
+def next_turn(current_turn):
+    print("Current Turn: " + current_turn)
+    if current_turn == "user":
+        return "comp_1"
+
+         self.hand = hand 
+    elif current_turn == "comp_1":
+        return "comp_2"
+    elif current_turn == "comp_2":
+        return "comp_3"
+    elif current_turn == "comp_3":
+        return "user"
+    else:
+        raise Exception("Fatal Error")
+
+
+class Player: 
+     
+    def __init__(self,hand):
+            self.hand = hand 
+
+#Peter
+import random
+
+#Create deck, shuffle 
+
+class Card:
+ SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+ VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+ 
+ def new_deck():
+        deck = []
+
+        for suit in Card.SUITS:
+            for value in Card.VALUES:
+                if not (value == 12 and suit == "Hearts"): # Exclude one card from deck (Queen of Hearts)
+        deck.append(Card(value, suit))
+#For each suit: For each value: If this is NOT the Queen of Hearts: Add this card to the deck
+        random.shuffle(deck)
+        return deck
+
+print("You got:")
+for i in range(1):
+   print(deck[i][0], "of", deck[i][1])
+   
+#Dictionary of named cards
+cards = {11: "Jack", 12: "Queen", 13: "King", 1:"Ace"}
+
+#Deal cards into hand
+def deal_cards(deck)
+    hands = [[], [], [], []]
+
+  for i in range(len(deck)):
+    hands[i % 4].append(deck[i])
+
+  return hands
+
+deck = new_deck()
+hands = deal_cards(deck)
+
+print("Player 1:", hands[0])
+print("Player 2:", hands[1]) 
+print("Player 3:", hands[2])
+print("Player 4:", hands[3])
+
+
+
+
+
+
+
+
